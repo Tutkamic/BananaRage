f970751b15e716750f39e163bce87b7b6dbc3d90
Michał <michal.tutka90@gmail.com>
2024-01-28 00:12:55 +0100

    Rules screen

--- BananaRage/Assets/Scripts/MainMenuController.cs
+++ BananaRage/Assets/Scripts/MainMenuController.cs
@@ -7,13 +7,20 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private Button buttonStart;
+    [SerializeField] private Button rulesScreenButton;
+    [SerializeField] private GameObject rulesScreen;
 
     private void Awake()
     {
+        rulesScreenButton.onClick.AddListener(OnRulesClickAndStartGame);
         buttonStart.onClick.AddListener(OnStartClick);
     }
 
     private void OnStartClick()
+    {
+        rulesScreen.SetActive(true);
+    }
+    private void OnRulesClickAndStartGame()
     {
         SceneManager.LoadScene(1);
     }
